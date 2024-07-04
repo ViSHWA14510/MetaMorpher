a4094aad03b4c6f30b8737962ff531a460344090
ViSHWA <154061854+ViSHWA14510@users.noreply.github.com>
2024-07-04 17:21:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = int(os.environ.get("API_ID", "10811400"))
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAGjSKZOl_kPksvKJNaKbiBl5XDI0tt5QpY")
-ADMIN = int(os.environ.get("ADMIN", '6469754522')) 
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
+API_ID = int(os.environ.get("API_ID", "27344784"))
+API_HASH = os.environ.get("API_HASH", "a12613f54c74d00bfb76cf8f0688c2ee")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7332689754:AAF8WZ3KT7sMEC6aKOclz5z_x9BnS8fzOhU")
+ADMIN = int(os.environ.get("ADMIN", '6899191648')) 
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "ViSHWA_MOViEX")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "Rashmika_Leech_Zone")
 CAPTION = os.environ.get("CAPTION", "")
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 DOWNLOAD_LOCATION = "./DOWNLOADS"
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+AUTH_USERS = int(os.environ.get("AUTH_USERS", '6899191648'))
 
 
 
